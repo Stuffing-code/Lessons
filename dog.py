6a6519e86ba5f73800250558dbb423e636c42f0b
Stuffcode <stuffingeu56@gmail.com>
2021-02-04 17:26:05 +0300

    class dog lessom completed

--- dog.py
+++ dog.py
@@ -6,12 +6,18 @@ class Dog():
         self.age = age
     def sit(self):
         """Собака садится по команде."""
-        print(f"{self.name} is now sitting.")
+        print(f"{self.name.title()} is now sitting.")
     def roll_over(self):
         """Собака перекатывается по команде."""
-        print(f"{self.name} rolled over!")
+        print(f"{self.name.title()} rolled over!")
 
 my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
 
-print(f"My dog's name is {my_dog.name}.")
-print(f"My dodg's is {my_dog.age} years old.")
+print(f"My dog's name is {my_dog.name.title()}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name.title()}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
